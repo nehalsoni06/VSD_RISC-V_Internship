6abbcc26b2945a5a884e7dbeed7e1edd7818eb39
nehalsoni06 <24uec191@lnmiit.ac.in>
2026-06-03 21:37:17 +0530

    add fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int n = 10, a = 0, b = 1, c;
+
+    printf("Fibonacci Series: ");
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
